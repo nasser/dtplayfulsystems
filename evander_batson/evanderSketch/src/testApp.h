605e94d2531507ca5e343e631cd8c1f731236d6b
Evander Batson <evanderbatson@gmail.com>
2014-02-19 17:53:12 -0500

    pushing it for real this time

--- evander_batson/evanderSketch/src/testApp.h
+++ evander_batson/evanderSketch/src/testApp.h
@@ -18,9 +18,5 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-    
-        int degreeShift;
-        int degreeRotate;
-        bool start;
 		
 };
