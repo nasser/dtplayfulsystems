8a30a07cde705ce66433fce68d8ab59c0930b5a3
fitosegrera <fitosegrera@gmail.com>
2014-02-19 04:15:38 -0500

    fractal finished

--- fitosegrera/fractal/src/testApp.cpp
+++ fitosegrera/fractal/src/testApp.cpp
@@ -2,7 +2,7 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
-
+    myfont.loadFont("coders_crux.ttf", 12);
 }
 
 //--------------------------------------------------------------
@@ -13,11 +13,31 @@ void testApp::update(){
 //--------------------------------------------------------------
 void testApp::draw(){
     ofBackground(0, 0, 0);
-    ofNoFill();
     ofSetColor(255, 255, 255);
-    ofSetRectMode(OF_RECTMODE_CENTER); //set rectangle mode to the center
-    ofRect(ofGetWidth()/2, ofGetHeight()/2, 500, 500);
+    myfont.drawString("Intricate Geometric Shape", 25,30);
+    myfont.drawString("CREATIVE CODING CLASS DT", 25,42);
+    myfont.drawString("Parsons, The New School", 25,54);
+    myfont.drawString("New York - 02/18/2014", 25,66);
+    myfont.drawString("fito_segrera", 25,78);
+    ofNoFill();
 
+    for(int i = 0; i <500; i+=10){
+        ofLine(leftPointX + i, leftPointY - i/factor, topPointX + i/factor, topPointY + i);
+        ofLine(topPointX + i/factor, topPointY + i, rightPointX - i, rightPointY + i/factor);
+        ofLine(rightPointX - i, rightPointY + i/factor, bottomPointX - i/factor, bottomPointY - i);
+        ofLine(bottomPointX - i/factor, bottomPointY - i, leftPointX + i, leftPointY - i/factor);
+    }
+    
+    for(int i = 0; i <500; i+=10){
+        ofLine(leftPointX2 + i, leftPointY2 - i/factor2, topPointX2 + i/factor2, topPointY2 + i);
+        ofLine(topPointX2 + i/factor2, topPointY2 + i, rightPointX2 - i, rightPointY2 + i/factor2);
+        ofLine(rightPointX2 - i, rightPointY2 + i/factor2, bottomPointX2 - i/factor2, bottomPointY2 - i);
+        ofLine(bottomPointX2 - i/factor2, bottomPointY2 - i, leftPointX2 + i, leftPointY2 - i/factor2);
+    }
+    
+    for(float i = 0; i <14; i+=0.5){
+        ofEllipse(ofGetWidth()/2, ofGetHeight()/2, i*i, i*i);
+    }
 }
 
 //--------------------------------------------------------------
--- fitosegrera/fractal/src/testApp.h
+++ fitosegrera/fractal/src/testApp.h
@@ -18,6 +18,28 @@ class testApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
-		
     
+    ofTrueTypeFont myfont;
+		
+    int rectSize = 500;
+    int factor = 5;
+    int factor2 = 10;
+    
+    int leftPointX = ofGetWidth()/2 - rectSize / 2;
+    int leftPointY = ofGetHeight()/2 - rectSize / 2;
+    int topPointX = ofGetWidth()/2 + rectSize / 2;
+    int topPointY = ofGetHeight()/2 - rectSize / 2;
+    int rightPointX = ofGetWidth()/2 + rectSize / 2;
+    int rightPointY = ofGetHeight()/2 + rectSize / 2;
+    int bottomPointX = ofGetWidth()/2 - rectSize / 2;
+    int bottomPointY = ofGetHeight()/2 + rectSize / 2;
+    
+    int leftPointX2 = ofGetWidth()/2 - rectSize / 4;
+    int leftPointY2 = ofGetHeight()/2 - rectSize / 4;
+    int topPointX2 = ofGetWidth()/2 + rectSize / 4;
+    int topPointY2 = ofGetHeight()/2 - rectSize / 4;
+    int rightPointX2 = ofGetWidth()/2 + rectSize / 4;
+    int rightPointY2 = ofGetHeight()/2 + rectSize / 4;
+    int bottomPointX2 = ofGetWidth()/2 - rectSize / 4;
+    int bottomPointY2 = ofGetHeight()/2 + rectSize / 4;
 };
