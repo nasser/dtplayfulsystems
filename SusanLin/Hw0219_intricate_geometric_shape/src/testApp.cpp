8969fcd92d81e05b559f6272619bd905093d6ba4
shuyl105 <shuyl105@newschool.edu>
2014-02-19 23:45:30 -0500

    Update

--- SusanLin/Hw0219_intricate_geometric_shape/src/testApp.cpp
+++ SusanLin/Hw0219_intricate_geometric_shape/src/testApp.cpp
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------
 void testApp::setup(){
     ofSetWindowShape(500, 500);
-    ofBackground(255, 255, 255);
+    ofBackground(0, 0, 0);
     
 }
 
@@ -13,9 +13,26 @@ void testApp::update(){
 }
 
 void drawlines(){
-    ofTranslate(100, 100);
     ofRotate(100);
-    ofLine(250, 250, 50, 50);
+    ofLine(20, 20, 100, 100);
+}
+
+void drawlines2(){
+    ofTranslate(0, 0);
+    ofRotate(50);
+    ofLine(0, 0, 100, 100);
+}
+
+void drawCricle(){
+    ofTranslate(0, 0);
+    ofRotate(5);
+    ofLine(10, 10, 50, 50);
+}
+
+void drawBigCricle(){
+    ofTranslate(0, 0);
+    ofRotate(5);
+    ofLine(100, 100, 500, 500);
 }
 
 //--------------------------------------------------------------
@@ -23,14 +40,28 @@ void testApp::draw(){
     
     ofSetColor(255, 95, 150);
     ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
-    ofLine(0, 0, 100, 100);
     
-    
-    for(int i=0; i<50; i++){
+//frist one
+    for(int i=0; i<60; i++){
         drawlines();
     }
-
-
+    
+// second
+    ofSetColor(95, 150, 255);
+    for (int a=0; a<100; a+=5) {
+        drawlines2();
+    }
+// Green
+    ofSetColor(0, 255, 95);
+    for (int a=0; a<500; a+=5) {
+        drawCricle();
+    }
+//White
+    ofSetColor(255, 255, 255);
+    for (int a=0; a<500; a+=5) {
+        drawBigCricle();
+    }
+    
 }
 
 //--------------------------------------------------------------
