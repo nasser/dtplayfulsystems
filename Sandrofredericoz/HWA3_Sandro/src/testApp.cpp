92649532eed3ad23414a09468f8f3a1ef1662356
Sandrofredericoz <sandro@sandros-ibook-g4.local>
2014-03-08 16:33:32 -0500

    Committing last two weeks of work

---
+++ Sandrofredericoz/HWA3_Sandro/src/testApp.cpp
@@ -0,0 +1,102 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+
+}
+
+//--------------------------------------------------------------
+
+
+void testApp:: drawStar(float x, float y, float size){
+  
+    
+    //draws star
+    /*
+    ofLine(x+0, y+100, x+100, y+100);
+    ofLine(x+100, y+100, x+150, y+0);
+    ofLine(x+150, y+0, x+200, y+100);
+    ofLine(x+200, y+100, x+300, y+100);
+    ofLine(x+300, y+100, x+200, y+150);
+    ofLine(x+200, y+150, x+250, y+250);
+    ofLine(x+250, y+250, x+150, y+200);
+    ofLine(x+150, y+200, x+50, y+250);
+    ofLine(x+50, y+250, x+100, y+150);
+    ofLine(x+100, y+150, x+0, y+100);
+    */
+    
+    
+    ofBeginShape();
+    ofVertex(x + 0, y + 100);
+    ofVertex(x + 100, y + 100);
+    ofVertex(x + 150, y + 0);
+    ofEndShape();
+}
+
+
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    
+    ofBackground(0, 0, 0);
+ 
+    // write code in curly brackets .. stays within the loop and does this code all the time
+    
+    for (int i=10; i<100; i++){
+        
+        drawStar(i*10, i*7.5, 100);
+    }
+    
+   
+        
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
