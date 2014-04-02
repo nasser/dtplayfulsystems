fb006f4b52c1a10a062d1b4e9b6d8a4f0d77926f
sabotai <sabotaione@gmail.com>
2014-04-02 18:42:20 -0400

    added midterm

---
+++ Alec_McClure/midterm_compositionalAnalysis/obj/linux64/Debug/OLD/ofxCv/src/Distance.d
@@ -0,0 +1,4 @@
+obj/linux64/Debug/OLD/ofxCv/src/Distance.o: OLD/ofxCv/src/Distance.cpp \
+ OLD/ofxCv/include/ofxCv/Distance.h
+
+OLD/ofxCv/include/ofxCv/Distance.h:
---
+++ Alec_McClure/midterm_compositionalAnalysis/obj/linux64/Debug/ofxCv/src/Distance.d
@@ -0,0 +1,4 @@
+obj/linux64/Debug/ofxCv/src/Distance.o: ofxCv/src/Distance.cpp \
+ ofxCv/include/ofxCv/Distance.h
+
+ofxCv/include/ofxCv/Distance.h:
